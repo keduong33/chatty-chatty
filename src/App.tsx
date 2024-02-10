d5251db85a16ed238b9c86cf6b1ec570ffe40b94
Ke Duong <kelyduong@gmail.com>
2024-02-10 21:24:06 +1100

    style: center & remove underline for page title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-xl font-bold text-center">
+        Chatty Chatty & Learn Langy
+      </h1>
       <LanguageSelector
         selectedLanguage={selectedLanguage}
         setSelectedLanguage={setSelectedLanguage}
