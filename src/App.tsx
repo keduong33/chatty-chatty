6ea7b3b18f37b208cf7808539f978eb87c3b12cd
Ke Duong <kelyduong@gmail.com>
2024-02-10 21:03:09 +1100

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { LanguageSelector } from "./components/LanguageSelector";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <LanguageSelector />
-    </>
+      <Button>Test</Button>
+    </ThemeProvider>
   );
 }
 
